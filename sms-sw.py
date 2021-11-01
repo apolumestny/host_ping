50de1abcf9ac8dfef0931dbc3beabb5e2b1036e9
apolumestny <anpolumestny@gmail.com>
2021-11-01 15:58:49 +0300

    flake8

--- sms-sw.py
+++ sms-sw.py
@@ -5,8 +5,6 @@ import datetime as dt
 import json
 import os
 import subprocess
-import sys
-
 import requests
 
 from typing import List, Union
@@ -24,8 +22,16 @@ else:
 
 class Alarm:
 
-    def __init__(self, call_url: str, sms_url: str, sms_user: str, sms_password: str, msisdn_to_notify: List[str],
-                 notification_delay: float, call_count: int,  *args, **kwargs):
+    def __init__(self,
+                 call_url: str,
+                 sms_url: str,
+                 sms_user: str,
+                 sms_password: str,
+                 msisdn_to_notify: List[str],
+                 notification_delay: float,
+                 call_count: int,
+                 *args,
+                 **kwargs):
         self.call_url = call_url
         self.sms_url = sms_url
         self.sms_user = sms_user
@@ -50,14 +56,24 @@ class Alarm:
 
     def send_sms_alarm(self, host: str) -> None:
         for msisdn in self.msisdn_to_notify:
-            url = self.sms_url.format(self.sms_user, self.sms_password, msisdn, f'Host {host} become unreachable')
+            url = self.sms_url.format(self.sms_user,
+                                      self.sms_password,
+                                      msisdn,
+                                      f'Host {host} become unreachable')
             requests.post(url)
 
 
 class DB(Alarm):
 
-    def __init__(self, hosts: List[str], msisdn_to_notify: List[str],
-                 notification_delay: float, call_count: int, call_url: str, sms_urlm: str, *args, **kwargs) -> None:
+    def __init__(self,
+                 hosts: List[str],
+                 msisdn_to_notify: List[str],
+                 notification_delay: float,
+                 call_count: int,
+                 call_url: str,
+                 sms_urlm: str,
+                 *args,
+                 **kwargs) -> None:
         self.db: str = os.path.join(basedir, 'state.txt')
         if not os.path.isfile(self.db):
             # create new db file
@@ -75,8 +91,13 @@ class DB(Alarm):
             if compare_host['delete_host'] is not None:
                 for delete_host in compare_host['delete_host']:
                     self.delete_host(delete_host)
-        super().__init__(msisdn_to_notify=msisdn_to_notify, notification_delay=notification_delay, call_count=call_count,
-                         call_url=call_url, sms_url=sms_urlm, sms_user=sms_user, sms_password=sms_password)
+        super().__init__(msisdn_to_notify=msisdn_to_notify,
+                         notification_delay=notification_delay,
+                         call_count=call_count,
+                         call_url=call_url,
+                         sms_url=sms_urlm,
+                         sms_user=sms_user,
+                         sms_password=sms_password)
 
     def read_db(self) -> dict:
         with open(self.db, 'r') as f:
@@ -94,7 +115,8 @@ class DB(Alarm):
         return resp
 
     def compare_host_from_db_and_config(self, hosts: list) -> dict:
-        # return difference between list of host in existing db and hosts from parameter
+        # return difference between list of
+        # host in existing db and hosts from parameter
         existing_host: set = set(host for host in self.read_db().keys())
         host: set = set(host for host in hosts)
         new_host: set = set(host - existing_host)
@@ -123,8 +145,10 @@ class DB(Alarm):
             db[host][key] = kwargs[key]
         self.save_db(db)
 
-    def notification_delay_check(self, notification_last_time: dt.datetime) -> bool:
-        notification_delay: float = (dt.datetime.now() - notification_last_time).seconds / 60
+    def notification_check(self,
+                           notification_last_time: dt.datetime) -> bool:
+        notification_delay: float = (dt.datetime.now()
+                                     - notification_last_time).seconds / 60
         if notification_delay >= self.notification_delay:
             return True
         return False
@@ -133,28 +157,32 @@ class DB(Alarm):
         date_format: str = '%Y-%m-%d %H:%M:%S.%f'
         current_time: dt.datetime = dt.datetime.now()
         for host, value in ping_result.items():
-            info: dict = self.get_value(host, keys=['current_state', 'call_count', 'notification_last_time', ])
+            info: dict = self.get_value(host,
+                                        keys=['current_state',
+                                              'call_count',
+                                              'notification_last_time', ])
             # if ping result equal 1 then host is unreachable
             # new alarm
             if value == 1 and info['current_state'] == 0:
-                # self.send_call_alarm(host)
-                # self.change_db_parameter(host, current_state=1, call_count=1,
-                #                          notification_last_time=current_time, check_time=current_time)
-                self.change_db_parameter(host, current_state=1, check_time=current_time)
+                self.change_db_parameter(host,
+                                         current_state=1,
+                                         check_time=current_time)
                 continue
             if value == 1 and info['current_state'] == 1:
                 self.send_call_alarm(host)
-                # self.change_db_parameter(host, current_state=1, call_count=1,
-                #                          notification_last_time=current_time, check_time=current_time)
-                self.change_db_parameter(host, current_state=1, unavailable=1,
-                                         notification_last_time=current_time, check_time=current_time)
+                self.change_db_parameter(host,
+                                         current_state=1,
+                                         unavailable=1,
+                                         notification_last_time=current_time,
+                                         check_time=current_time)
                 continue
             # host was unreachable
             elif value == 1 and info['unavailable'] == 1:
-                notification_last_time: dt.datetime = dt.datetime.strptime(info['notification_last_time'], date_format)
+                last_notif_time: dt.datetime = dt.datetime.strptime(info['notification_last_time'], date_format)
                 # check call count if it not equal count in conf
                 # and call_delay more than in conf than make call
-                if info['call_count'] < self.call_count and self.notification_delay_check(notification_last_time):
+                is_notification = self.notification_check(last_notif_time)
+                if info['call_count'] < self.call_count and is_notification:
                     self.send_call_alarm(host)
                     self.change_db_parameter(host,
                                              current_state=1,
@@ -164,9 +192,9 @@ class DB(Alarm):
                                              unavailable=1)
 
                 # if call count equal count in conf than check sms_last_time
-                # and if it None send sms if not None - get sms_delay if it equal or
-                # more than sms_delay from conf than send sms
-                elif info['call_count'] == self.call_count and self.notification_delay_check(notification_last_time):
+                # and if it None send sms if not None - get sms_delay
+                # if it equal or more than sms_delay from conf than send sms
+                elif info['call_count'] == self.call_count and is_notification:
                     self.send_sms_alarm(host)
                     self.change_db_parameter(host,
                                              current_state=1,
@@ -197,7 +225,8 @@ class Server:
     def ping_servers(self) -> dict:
         result: dict = {}
         for host in self.hosts:
-            proc: subprocess.Popen = subprocess.Popen(['ping', '-W', '1', '-c', '1', host], stdout=subprocess.PIPE)
+            cmd = ['ping', '-W', '1', '-c', '1', host]
+            proc: subprocess.Popen = subprocess.Popen(cmd, stdout=subprocess.PIPE)
             stdout, stderr = proc.communicate()
             result[host] = proc.returncode
         return result
@@ -218,7 +247,15 @@ class Server:
                 min, avg, max = stat.split(',')[1].split('=')[1].strip().split('/')
             except IndexError:
                 min, avg, max = [None, None, None]
-            resp.append([dt.datetime.utcnow(),'ip_from', host.strip(), sent, received, loss.replace('%', ''), min, avg, max,])
+            resp.append([dt.datetime.utcnow(),
+                         'ip_from',
+                         host.strip(),
+                         sent,
+                         received,
+                         loss.replace('%', ''),
+                         min,
+                         avg,
+                         max, ])
         return json.dumps(resp, default=str)
 
 
@@ -232,13 +269,19 @@ if __name__ == '__main__':
     sms_user = config['notification']['sms_username']
     sms_password = config['notification']['sms_password']
 
-    db = DB(hosts=hosts, call_count=call_count, notification_delay=notification_delay,
-            msisdn_to_notify=msisdn, call_url=call_url, sms_urlm=sms_url, sms_user=sms_user, sms_password=sms_password)
+    db = DB(hosts=hosts,
+            call_count=call_count,
+            notification_delay=notification_delay,
+            msisdn_to_notify=msisdn,
+            call_url=call_url,
+            sms_urlm=sms_url,
+            sms_user=sms_user,
+            sms_password=sms_password)
 
     server = Server(hosts)
     # you can check logic of alarm by edit result var
     # result = {'host_ip': state[0 - all good, 1 host unavailable]}
-    result = {'9.19.19.19': 1}
-    #result = server.ping_servers()
-    #print(server.fping_result_to_json(result))
+    # result = {'9.19.19.19': 1}
+    result = server.ping_servers()
+    # print(server.fping_result_to_json(result))
     db.check_ping_result(result)
